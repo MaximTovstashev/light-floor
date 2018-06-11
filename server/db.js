9dc463497f031f74c9e7b84c50a4bd24b9e89a88
Kitsch <max.kitsch@gmail.com>
2018-06-11 19:34:11 +0300

    Switched to Vue-Electron

--- server/db.js
+++
@@ -1,5 +0,0 @@
-const env       = process.env.ENV || 'packed';
-const config    = require('../knexfile');
-const knex      = require('knex')(config[env]);
-
-module.exports = knex;
--- server/index.js
+++
@@ -1,7 +0,0 @@
-const Brest = require('brest');
-const settings = require('./settings');
-const brest = new Brest(settings);
-
-require('./db');
-
-brest.on('ready',()=>{});
